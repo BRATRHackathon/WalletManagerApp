3086eea827b4a50195df77d9793fe2135502a0fa
Evandro Goncalves Junior <efgjunior@gmail.com>
2016-12-16 14:58:47 -0200

    Added Wallet address and seed information.

--- WalletManagerApp/App_Start/WebApiConfig.cs
+++ WalletManagerApp/App_Start/WebApiConfig.cs
@@ -1,6 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace WalletManagerApp
@@ -10,6 +11,7 @@ namespace WalletManagerApp
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            config.Formatters.Add(new BrowserJsonFormatter());
 
             // Web API routes
             config.MapHttpAttributeRoutes();
@@ -19,6 +21,21 @@ namespace WalletManagerApp
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+        }
+
+        public class BrowserJsonFormatter : JsonMediaTypeFormatter
+        {
+            public BrowserJsonFormatter()
+            {
+                this.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+                this.SerializerSettings.Formatting = Formatting.Indented;
+            }
+
+            public override void SetDefaultContentHeaders(Type type, HttpContentHeaders headers, MediaTypeHeaderValue mediaType)
+            {
+                base.SetDefaultContentHeaders(type, headers, mediaType);
+                headers.ContentType = new MediaTypeHeaderValue("application/json");
+            }
         }
     }
 }
--- WalletManagerApp/Controllers/WalletController.cs
+++ WalletManagerApp/Controllers/WalletController.cs
@@ -2,18 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using WalletManagerApp.Models;
 
 namespace WalletManagerApp.Controllers
 {
-    public class WalletController : Controller
+    public class WalletController : ApiController
     {
-        [HttpPost]
-        public ActionResult Create(WalletViewModel viewModel)
+        //http://localhost:46664/api/WalletManager/GetWalletByEmail?email=danielctrl@gmail.com
+        public IHttpActionResult GetWalletByEmail(string email)
         {
-            // SAVE viewModel data
-            return View();
+            HttpContext.Current.Response.AddHeader("Access-Control-Allow-Origin", "*");
+
+            var wallet = (new ApplicationDbContext()).Users.Where(x => x.Email == email).Select(x => x.WalletAddress).FirstOrDefault();
+
+            if (string.IsNullOrWhiteSpace(wallet))
+                return Json("Nada encontrado");
+            //return Content(HttpStatusCode.NoContent, "");
+
+            return Json(wallet);
+        }
+
+        //http://localhost:46664/api/WalletManager/GetEmailByWallet?wallet=0x11
+        public IHttpActionResult GetEmailByWallet(string wallet)
+        {
+            var email = (new ApplicationDbContext()).Users.Where(x => x.WalletAddress == wallet).Select(x => x.Email).FirstOrDefault();
+
+            if (string.IsNullOrWhiteSpace(email))
+                return Json("Nada encontrado");
+            //return Content(HttpStatusCode.NoContent, "");
+
+            HttpContext.Current.Response.AddHeader("Access-Control-Allow-Origin", "*");
+            return Json(email);
         }
     }
 }
