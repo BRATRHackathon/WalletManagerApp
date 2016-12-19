8254ddb36806cc081038e2bc4caf232099d9d362
Eduardo Saleh <eduardo.saleh@thomsonreuters.com>
2016-12-19 19:06:25 -0200

    fixing find wallets problems

--- WalletManagerApp/Controllers/WalletManagerController.cs
+++ WalletManagerApp/Controllers/WalletManagerController.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
 using System.Web.Http;
-using System.Web.Mvc;
 using WalletManagerApp.Models;
 
 namespace WalletManagerApp.Controllers
@@ -12,9 +8,7 @@ namespace WalletManagerApp.Controllers
     {
         //http://localhost:46664/api/WalletManager/GetWalletByEmail?email=danielctrl@gmail.com
         public IHttpActionResult GetWalletByEmail(string email)
-        {
-            HttpContext.Current.Response.AddHeader("Access-Control-Allow-Origin", "*");
-
+        {            
             var wallet = (new ApplicationDbContext()).Users.Where(x => x.Email == email).Select(x => x.WalletAddress).FirstOrDefault();
 
             if (string.IsNullOrWhiteSpace(wallet))
@@ -25,14 +19,19 @@ namespace WalletManagerApp.Controllers
 
         //http://localhost:46664/api/WalletManager/GetEmailByWallet?wallet=0x11
         public IHttpActionResult GetEmailByWallet(string wallet)
-        {
-            var email = (new ApplicationDbContext()).Users.Where(x => x.WalletAddress == wallet).Select(x => x.Email).FirstOrDefault();
-
+        {            
+            var email = (new ApplicationDbContext())
+                .Users
+                .Where(x => 
+                    x.WalletAddress == wallet.Replace("0x","") ||
+                    x.WalletAddress == wallet
+                )
+                .Select(x => x.Email)
+                .FirstOrDefault();
+            
             if (string.IsNullOrWhiteSpace(email))
-                return NotFound();
-            //return Content(HttpStatusCode.NoContent, "");
-
-            HttpContext.Current.Response.AddHeader("Access-Control-Allow-Origin", "*");
+                return NotFound();            
+                        
             return Ok(email);
         }
     }
